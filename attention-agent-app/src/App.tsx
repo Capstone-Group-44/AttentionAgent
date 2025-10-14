fb009e886c938954f8385bbc9ebd276c7efb3f2c
sarahalsaady <148502402+salsaady@users.noreply.github.com>
2025-10-14 16:49:21 -0400

    auth demo

---
+++ attention-agent-app/src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { initializeApp } from 'firebase/app';
+import { config } from './config/config';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthRoute from './components/AuthRoute';
+import HomePage from './pages/Home';
+import LoginPage from './pages/Login';
+
+initializeApp(config.firebaseConfig);
+
+function App() {
+  return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <AuthRoute>
+                            <HomePage />
+                        </AuthRoute>
+                    }
+                />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App
