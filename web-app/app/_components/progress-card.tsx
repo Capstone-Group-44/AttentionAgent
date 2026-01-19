af2cea57e23414fa34030ad4683891a6548c84f8
sarahalsaady <148502402+salsaady@users.noreply.github.com>
2026-01-19 16:05:27 -0500

    add stat rings with dummy values

--- web-app/app/_components/progress-card.tsx
+++ web-app/app/_components/progress-card.tsx
@@ -1,14 +1,45 @@
 'use client'
 
+import { StatRing } from "@/components/stat-ring";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { formatDuration } from "@/lib/utils";
 
 export function ProgressCard() {
+  const focusSecondsToday = 10800 // Example: 3 hours in seconds
+  const numberSessionsToday = 5
+  const avgFocusScoreToday = 85 // Example: 85%
   return (
-    <Card className="flex mx-auto w-250 h-75 p-6 shadow">
+    <Card className="">
     <CardHeader>
             <CardTitle>Today's Progress</CardTitle>
         </CardHeader>
         <CardContent>
+          <div className="grid gap-6 sm:grid-cols-3 justify-items-center">
+
+          <StatRing label={"Focus Time"} value={formatDuration(focusSecondsToday)} percent={0}/>
+          <StatRing
+            label="Sessions"
+            value={
+              <span>
+                {numberSessionsToday}
+                <span> Completed</span>
+              </span>
+            }
+            percent={0}
+          />
+
+          <StatRing
+            label="Average Focus Score"
+            value={
+              <span>
+                {avgFocusScoreToday}
+                <span> / 100</span>
+              </span>
+            }
+            percent={0}
+          />
+
+          </div>
         </CardContent>
     </Card>
 
